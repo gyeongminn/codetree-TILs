533717e5280208b3cff111391b3f27f9474e4561
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-12-05 23:55:08 +0900

    Title: 황금비율 토스트, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231205/황금비율 토스트/golden-toast.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <list>
+#include <string>
+using namespace std;
+
+int n, m;
+string s;
+list<char> bread;
+list<char>::iterator it;
+
+void solve() {
+    char opt; cin >> opt;
+    if (opt == 'L') {
+        if (it != bread.begin()) it--;
+    } else if (opt == 'R') {
+        if (it != bread.end()) it++;
+    } else if (opt == 'D') {
+        if (it != bread.end()) it = bread.erase(++it);
+    } else if (opt == 'P') {
+        char c; cin >> c;
+        it = bread.insert(it, c);
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    cin >> s;
+    for (char c : s) bread.push_back(c);
+    it = bread.end();
+
+    while (m--) solve();
+
+    for (it = bread.begin(); it != bread.end(); it++) {
+        cout << *it;
+    }
+
+    return 0;
+}
