21f13cca723730457a01ddf00c456b46842a675b
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-12-07 23:10:39 +0900

    Title: 중앙값 계산 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231207/중앙값 계산 2/get-median-2.cpp
+++ 231207/중앙값 계산 2/get-median-2.cpp
@@ -5,11 +5,14 @@ int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr);
     int n;
     cin >> n;
-    vector<int> v(n+1);
+    vector<int> v;
     for(int i = 1; i <= n; i++) {
-        cin >> v[i];
+        int a;
+        cin >> a;
+        v.push_back(a);
         if (i & 1) {
-            cout << v[(i + 1) / 2] << ' ';
+            nth_element(v.begin(), v.begin() + i / 2 , v.end());
+            cout << v[i / 2] << ' ';
         }
     }
     return 0;
