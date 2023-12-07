c48c53ca80af2847a1ee90d376823dc31b51b6d7
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-12-07 22:33:47 +0900

    Title: 두 정수에 대한 연산값 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231207/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
+++ 231207/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main() {
     int a, b;
     cin >> a >> b;
-    if (a > b) {
+    if (a < b) {
         a += 10;
         b *= 2;
     } else {
         a *= 2;
         b += 10;
     }
-    cout << a << b;
+    cout << a << ' ' << b;
     return 0;
 }
