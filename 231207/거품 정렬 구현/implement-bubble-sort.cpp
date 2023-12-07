e653a6543aa7f844f0cd7075d5d2c5db27662d84
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2023-12-07 16:35:10 +0900

    Title: 거품 정렬 구현, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231207/거품 정렬 구현/implement-bubble-sort.cpp
+++ 231207/거품 정렬 구현/implement-bubble-sort.cpp
@@ -8,8 +8,8 @@ int main() {
     vector<int> v(n);
     for (auto& i : v) cin >> i;
 
-    for (int i = 0; i < v.size() - 1; i++) {
-        for (int j = 0; j < v.size(); j++) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1; j++) {
             if (v[j] > v[j + 1]) {
                 int tmp = v[j];
                 v[j] = v[j + 1];
